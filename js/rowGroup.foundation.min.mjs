daa5117da48f446b38363c9eb9151379bf02b851
dtbuild <dtbuild@rigil>
2026-05-07 13:51:31 +0000

    8183b71a55322d9cf9245633002da19c0b7e9b12
    Sync to source repo @1d80efc25e05b80d202aa5699e2950417ecaf4ef

--- js/rowGroup.foundation.min.mjs
+++ js/rowGroup.foundation.min.mjs
@@ -1,4 +1,4 @@
-/*! Foundation styling wrapper for RowGroup
- * © SpryMedia Ltd - datatables.net/license
+/*! RowGroup Foundation styling 2.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
-import jQuery from"jquery";import DataTable from"datatables.net-zf";import RowGroup from"datatables.net-rowgroup";let $=jQuery;export default DataTable;
+import DataTable from"datatables.net-zf";import RowGroup from"datatables.net-rowgroup";export default DataTable;
